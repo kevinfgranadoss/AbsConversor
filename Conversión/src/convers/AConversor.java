cca60e288a4d4977b5a8d9fc2aaff9f6faf32924
user <user@Kevin>
2017-10-18 15:53:34 -0500

    Creadas las clases AConversor e ITransformador

---
+++ Conversión/src/convers/AConversor.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convers;
+
+public class AConversor {
+    double dato;
+    abstract double grado2radian();
+    abstract double farenheit2kelvin();
+}
