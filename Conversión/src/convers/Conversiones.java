b1da9fa4a2f290bcb04c791e639777d1c2b72278
user <user@Kevin>
2017-10-19 09:38:23 -0500

    Modificación de "estilo"

--- Conversión/src/convers/Conversiones.java
+++ Conversión/src/convers/Conversiones.java
@@ -13,13 +13,17 @@ public class Conversiones {
     
     public static void main(String[] args){
         ConversorC miConversor = new ConversorC();
-        //miConversor.dato=80.24;
         
-        System.out.println(miConversor.farenheit2kelvin());
-        System.out.println(miConversor.grado2radian());
+        miConversor.dato=80.24;
+        
+        System.out.println("De Farenheit a Kelvin: "+miConversor.farenheit2kelvin());
+        System.out.println("De Grado a Radian: "+miConversor.grado2radian());
+        
+        
         
         ConversorI miInt=new ConversorI();
-        System.out.println(miInt.grado2radian());
-        System.out.println(miInt.kelvin2celsius());
+        miInt.valor=38.58;
+        System.out.println("De Grado a Radian: "+miInt.grado2radian());
+        System.out.println("De Kelvin a Celsius:"+miInt.kelvin2celsius());
     }
 }
